3da6af4d5123d651378d67d75dbbce98a11f8af0
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-09-13 16:29:14 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Eric on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
