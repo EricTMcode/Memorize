3c1a153f622a717589ee66e0b5fffe9d9ec5dae2
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-09-18 06:12:50 +0200

    Add init to AspectVGrid

--- Memorize/AspectVGrid.swift
+++ Memorize/AspectVGrid.swift
@@ -10,7 +10,13 @@ import SwiftUI
 struct AspectVGrid<Item: Identifiable, ItemView: View>: View {
     var items: [Item]
     var aspectRatio: CGFloat = 1
-    var content: (Item) -> ItemView
+    @ViewBuilder var content: (Item) -> ItemView
+
+    init(_ items: [Item], aspectRatio: CGFloat, content: @escaping (Item) -> ItemView) {
+        self.items = items
+        self.aspectRatio = aspectRatio
+        self.content = content
+    }
 
     var body: some View {
         GeometryReader { geo in
