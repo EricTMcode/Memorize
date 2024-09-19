2c8014c3b1229f3adf0c2ea87dc48d406bbd9a5a
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-09-19 06:40:46 +0200

    Create extension on animation

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -27,7 +27,7 @@ struct CardView: View {
                     .aspectRatio(1, contentMode: .fit)
                     .padding(Constants.Pie.inset)
                     .rotationEffect(.degrees(card.isMatched ? 360 : 0))
-                    .animation(.linear(duration: 1).repeatForever(autoreverses: false), value: card.isMatched)
+                    .animation(.spin(duration: 1), value: card.isMatched)
             )
             .padding(Constants.inset)
             .cardify(isFaceUp: card.isFaceUp)
@@ -47,6 +47,12 @@ struct CardView: View {
             static let opacity: CGFloat = 0.4
             static let inset: CGFloat = 5
         }
+    }
+}
+
+extension Animation {
+    static func spin(duration: TimeInterval) -> Animation {
+        .linear(duration: 1).repeatForever(autoreverses: false)
     }
 }
 
